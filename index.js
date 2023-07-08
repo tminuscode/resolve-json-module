2f3c8a133998af8645b3a28a49f41f4b4a2261b7
tpluscode <tpluscode@users.noreply.github.com>
2023-07-08 17:42:32 +0200

    repro

---
+++ index.js
@@ -0,0 +1,2 @@
+import htmlTags from 'html-tags/html-tags.json' assert { type: 'json' };
+const keys = Object.keys(htmlTags);
---
+++ index.ts
@@ -0,0 +1,3 @@
+import htmlTags from 'html-tags/html-tags.json' assert { type: 'json' }
+
+const keys = Object.keys(htmlTags)
